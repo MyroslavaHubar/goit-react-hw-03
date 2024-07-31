b5cf5fa712823ca2efcf32fa18bf7a01bfe5fb66
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-07-31 22:31:12 +0200

    add all new files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Section from "./component/Section/Section";
+
+function App() {
+  return (
+    <>
+      <Section></Section>
+    </>
+  );
+}
+
+export default App;
